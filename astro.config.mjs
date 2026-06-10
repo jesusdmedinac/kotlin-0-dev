8fa945450e0571b2a548c2fe55fa3b012c48b6f7
Jesus Daniel Medina Cruz <jesus.daniel.medina.cruz@gmail.com>
2026-06-10 11:46:12 -0700

    feat: setup Starlight routing and folder structure for Course 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,19 +6,12 @@ import starlight from '@astrojs/starlight';
 export default defineConfig({
 	integrations: [
 		starlight({
-			title: 'My Docs',
-			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/withastro/starlight' }],
+			title: 'Desde0: Cursos Kotlin',
+			social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/jesusdmedinac/kotlin-0-dev' }],
 			sidebar: [
 				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					items: [{ autogenerate: { directory: 'reference' } }],
+					label: 'Curso 2: Principiantes',
+					autogenerate: { directory: 'course-2-beginners' },
 				},
 			],
 		}),
